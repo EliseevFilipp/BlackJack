ec307ab0fdf077c3036f3b0feeb437f4aac3992b
EliseevFilipp <eliseev.filipp@ya.ru>
2016-11-14 17:16:32 +0300

    Deck+Hand+наследование.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,11 @@
 package com.company;
 
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
-        for (Suit s: Suit.values())
-            for (Value v: Value.values()) {
-            Card c = new Card(s, v);
-                System.out.println(c);
-            }
-	    Suit m = Suit.CLUBS;
-        System.out.println(m);
-        Card c = new Card(Suit.SPADES, Value.ACE);
-        System.out.println(c);
+        Deck deck = new Deck();
+        System.out.println(deck);
     }
 }
