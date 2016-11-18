eb5a182417c30654af3dcecf5a0a8d309ae462df
EliseevFilipp <eliseev.filipp@ya.ru>
2016-11-18 17:26:41 +0300

    18.11.2016 Команды + начало игры. Описываем работу интеллекта по выбору действий.

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,7 +1,18 @@
 package com.company;
 
+import Command.Command;
+
 /**
  * Created by student1 on 14.11.16.
  */
 public abstract class Player {
+    public Hand hand = new Hand();
+
+    public void take(Card current) {
+        hand.add(current);
+    }
+
+    public Command decision() {
+        return intellect.decide();
+    }
 }
